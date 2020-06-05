5d6f99b2bba971d87aacbf73de20b6c5d9b0f232
airaou <597904453@qq.com>
2020-06-05 22:45:51 +0800

    添加翻译

---
+++ lang_zh-CN.ts
@@ -0,0 +1,369 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="17"/>
+        <source>解压自动化</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>压缩包列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="103"/>
+        <source>密码列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <source>输出目录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="169"/>
+        <source>开始解压</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="229"/>
+        <location filename="mainwindow.ui" line="265"/>
+        <source>帮助</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="236"/>
+        <source>语言</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <location filename="mainwindow.ui" line="242"/>
+        <source>配置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="46"/>
+        <source>清空列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <location filename="mainwindow.ui" line="113"/>
+        <source>移除选中</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>为压缩包各建文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="197"/>
+        <source>调试输出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>调试</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="262"/>
+        <source>帮助(&amp;H)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="270"/>
+        <source>关于(&amp;A)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="273"/>
+        <source>关于</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="278"/>
+        <source>加载QM语言包(&amp;Q)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="281"/>
+        <source>加载QM语言包</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="286"/>
+        <source>重新加载extractcfg.json</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="291"/>
+        <source>保存配置(&amp;S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="294"/>
+        <source>保存配置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="297"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="302"/>
+        <source>保存配置并退出(&amp;C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="305"/>
+        <source>保存配置并退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="308"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="313"/>
+        <source>调试输出(&amp;D)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="316"/>
+        <source>开启调试输出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="319"/>
+        <source>Ctrl+D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="34"/>
+        <location filename="mainwindow.cpp" line="497"/>
+        <source>Ready.</source>
+        <translation>就绪。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>Loading language file.</source>
+        <translation>加载语言文件中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="58"/>
+        <location filename="mainwindow.cpp" line="140"/>
+        <location filename="mainwindow.cpp" line="240"/>
+        <location filename="mainwindow.cpp" line="246"/>
+        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="mainwindow.cpp" line="271"/>
+        <location filename="mainwindow.cpp" line="339"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="59"/>
+        <location filename="mainwindow.cpp" line="241"/>
+        <source>Can not find %1.</source>
+        <translation>找不到文件 %1。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <source>Loading config file.</source>
+        <translation>加载配置文件中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="76"/>
+        <source>Info</source>
+        <translation>注意</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="77"/>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="482"/>
+        <location filename="mainwindow.cpp" line="483"/>
+        <source>Please choose an extractcfg.json config file.</source>
+        <translation>请选择一个 extractcfg.json 配置文件。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="116"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="116"/>
+        <source>%1 is not a folder.</source>
+        <translation>%1 不是文件夹。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="141"/>
+        <source>Save %1 failed.</source>
+        <translation>保存 %1 失败。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="145"/>
+        <source>Saving config file.</source>
+        <translation>保存配置文件中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="182"/>
+        <source>Loading passwords.</source>
+        <translation>加载密码表中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="200"/>
+        <source>Loading variables.</source>
+        <translation>加载变量中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="218"/>
+        <source>Loading name of programs.</source>
+        <translation>加载解压程序名称中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="236"/>
+        <source>Loading file %1.</source>
+        <translation>加载文件 %1 中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="247"/>
+        <source>Can not open %1.</source>
+        <translation>打不开文件 %1。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="261"/>
+        <source>Json syntax error.
+File: %1.
+Offset: %2.
+The context is shown below:
+
+</source>
+        <translation>JSON语法错误。
+文件：%1。
+位置：%2。
+错误处上下文如下：
+
+</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
+        <source>(&lt;- about here)</source>
+        <translation>(←大概在这里出错)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="272"/>
+        <source>Root should be an object.</source>
+        <translation>JSON文件的根对象应为Object。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Found %1 files.</source>
+        <translation>找到 %1 个文件。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="295"/>
+        <source>Release to append the file or set the output folder.</source>
+        <translation>释放鼠标以添加压缩包或设定输出文件夹或设定解压程序。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="317"/>
+        <source>set &apos;%1&apos; = &apos;%2&apos;.</source>
+        <translation>设定解压程序 &apos;%1&apos; = &apos;%2&apos;。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="339"/>
+        <source>Please provide a local file or a local folder.</source>
+        <translation>请至少提供一个本地的文件或文件夹。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="348"/>
+        <source>No archive to extract.</source>
+        <translation>没有要解压的压缩包。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="364"/>
+        <source>Handling archive %1.</source>
+        <translation>处理压缩包 %1 中。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="397"/>
+        <source>Found error: %1.</source>
+        <translation>发现错误：%1。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="449"/>
+        <source>Succeed: %1, failed: %2, password incorrect: %3, skiped: %4. Found new archive: %5.</source>
+        <translation>成功：%1，失败：%2，密码错误：%3，跳过：%4。发现新压缩包：%5。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="460"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="460"/>
+        <source>ABOUT_CONTENT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="465"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="465"/>
+        <source>HELP_CONTENT</source>
+        <translation>使用提示:
+1. 拖动一个或多个压缩包到本窗口作为输入
+2. 拖动文件夹到本窗口作为输出
+3. 双击密码列表添加密码
+4. 点击开始解压
+
+结果提示:
+1. 成功的会打上√标记
+2. 无法解析的压缩包的会打上×标记
+3. 密码错误会打上W标记
+4. 无法解析的会打上?标记
+5. 解压生成的文件均会进行探测，是压缩包的会自动添加到压缩包列表中以进行深层解压
+
+有以下方式提供密码:
+1. 在本程序内编辑输入提供（可在菜单处保存密码表）
+2. 将密码在压缩包文件名中以{和}包围提供</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="470"/>
+        <source>Please choose a qm language file.</source>
+        <translation>请选择一个QM语言文件。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="extracter.cpp" line="105"/>
+        <source>%1 is not an excutable program.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="extracter.cpp" line="134"/>
+        <source>Archive error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="extracter.cpp" line="141"/>
+        <source>Exit code incorrect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="extracter.cpp" line="149"/>
+        <source>Password error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
